40eff630d2748ca70ac22dba99c53fb1cf8c5215
robin hood <fordarnold@gmail.com>
2015-01-04 19:46:42 +0300

    Added authentication routes... Views coming next

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,52 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+/**
+ * Auth Routes
+ * @author robin hood <fordarnold@gmail.com>
+ */
+
+# Login, Logout
+Route::get('user/login', 'MasterUserController@getLogin');
+Route::post('user/login', 'MasterUserController@postLogin');
+Route::get('user/logout', 'MasterUserController@logout');
+
+# Register
+Route::get('user/register', 'MasterUserController@getRegister');
+Route::post('user/register', 'MasterUserController@postRegister');
+
+# Authenticate user before visiting these routes
+// Route::group(['before' => 'auth'], function(){
+
+	# Every logged-in user gets to:
+	# 
+	# Update his/her account
+	Route::get('user/update', 'MasterUserController@getUserUpdate');
+	Route::post('user/update', 'MasterUserController@postUserUpdate');
+
+	# 
+	
+
+// }
+
+# Authenticate user as 'master' before visiting these routes
+// Route::group(['before' => 'auth|master'], function(){
+
+	# ONLY master users can:
+	# 
+	# Register user as 'financialmanager'
+	Route::get('user/register/financialmanager', 'OtherUserController@getFinancialManagerRegister');
+	Route::post('user/register/financialmanager', 'OtherUserController@postFinancialManagerRegister');
+
+	# Delete user
+	Route::get('user/delete', 'MasterUserController@getUserCleanup');
+	Route::post('user/delete', 'MasterUserController@postUserCleanup');
+
+// }
+
+# Authenticate user as 'financialmanager' before visiting these routes
+// Route::group(['before' => 'auth|financialmanager'], function(){
+
+	// Route::post('user/delete', 'MasterUserController@postUserCleanup');
+
+// }
