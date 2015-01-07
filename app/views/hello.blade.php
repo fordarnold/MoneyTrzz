b269dbd84766626cab1c39e2d1c6cb69aeb128ba
robin hood <fordarnold@gmail.com>
2015-01-08 01:58:30 +0300

    Added Zurb's foundation files

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,75 +1,75 @@
 <!doctype html>
-<html lang="en">
+<html class="no-js" lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>Money Trzz - Welcome</title>
+
+	<!-- grid framework -->
+	{{ HTML::style('assets/dev/vendor/foundation-5.5.0/css/foundation.css') }}
 
 	{{ HTML::style('assets/dev/css/general.css') }}
 	{{ HTML::style('assets/dev/css/helpers.css') }}
 	{{ HTML::style('assets/dev/css/app.css') }}
 
-	<style>
+	{{ HTML::script('assets/dev/vendor/foundation-5.5.0/js/vendor/modernizr.js') }}
 
-		body {
-			text-align: center;
-			vertical-align: middle;
-			font-size: 1.2em;
-		}
-
-		.welcome {
-			width: 50%;
-			margin: 30px auto;
-			text-align: center;
-		}
-
-		a, a:visited {
-			text-decoration:none;
-		}
-
-		h1, h2, h3, h4, h5{font-family: "TakaoPGothic", sans-serif; }
-		h1, h2, h3{color: #444;}
-
-		.app-name {
-			font-size: 3em;
-			margin: 16px 0 0 0;
-		}
-		.app-slogan {
-			font-size: 1.5em;
-		}
-
-		h3 {color: limegreen;}
-
-		article ul {padding-left: 0;}
-
-		article ul li {list-style: none;}
-
-		span.special {color: green; font-size: 0.8em; font-family: "Times New Roman", serif;}
-	</style>
 </head>
 <body>
-	<div class="welcome">
-		<h1 class="app-name">Money Trzz</h1>
-		<h2 class="app-slogan">Easy money management<br>to get you out of Rat Race</h2>
 
-		<hr>
-		<h3>What does it give you ?</h3>
+	<header class="text-center">
+		<div class="row">
+			<div class="large-12 columns">
+				<hgroup>
+					<h1 class="app-name">
+						{{ HTML::link('/', 'Money Trzz') }}
+					</h1>
+					<h3 class="app-slogan">Easy money management<br>to get you out of Rat Race</h3>
+				</hgroup>
+			</div>
+		</div>
+	</header>
 
-		<article>
-			<ul>
-				<li>A Master user account</li>
-				<li>Multiple Business/Company accounts registration <span class="special">[Premium]</span></li>
-				<li>Multiple Bank accounts registration <span class="special">[Premium]</span></li>
-				<li>Categorised bank accounts like Personal or Business, Savings or Current or Checking</li>
-				<li>Deposits management</li>
-				<li>Withdrawals management</li>
-				<li>Savings management</li>
-				<li>Bank charge management</li>
-				<li>Money Reports, filtered by Quarter/Month</li>
-			</ul>
+	<main class="text-center">
+		<div class="row">
+			<div class="large-12 columns">
+				<hr>
+				<h3>What does it give you ?</h3>
 
-			<hr>
-			<h2>{{ HTML::link('index.php/user/register', 'Try it now !', array('class' => 'button')) }}</h2>
-		</article>
-	</div>
+				<article class="features">
+					<ul class="no-bullets">
+						<li>A Master user account</li>
+						<li>Multiple Business/Company accounts registration</li>
+						<li>Multiple Bank accounts registration </li>
+						<li>Categorised bank accounts like Personal or Business, Savings or Current or Checking</li>
+						<li>Deposits management</li>
+						<li>Withdrawals management</li>
+						<li>Savings management</li>
+						<li>Bank charge management</li>
+						<li>Money Reports, filtered by Quarter/Month</li>
+					</ul>
+
+						{{ HTML::link('index.php/user/register', 'Try it now !', array('class' => 'button round alert')) }}
+				</article>
+
+			</div>
+		</div>
+	</main>
+
+	<footer class="footer">
+		<div class="row">
+			<div class="large-6 columns">
+				<hr>
+				<ul class="inline-list">
+					<li>{{ HTML::link('legal/terms', 'Terms and conditions') }}</li>
+					<li>{{ HTML::link('legal/privacy', 'Privacy statement') }}</li>
+				</ul>
+			</div>
+			<div class="large-6 columns">
+				<hr>
+				<p class="copyright text-right">Copyright &copy; 2015, Meridian Appz Inc.</p>
+			</div>
+		</div>
+	</footer>
 </body>
 </html>
